39415534f42328c9d51f6dd82722992bd3c44d17
yinwm <yinweiming@gmail.com>
2010-09-15 19:05:51 +0800

    add mouse

--- emacs/elisp/base.el
+++ emacs/elisp/base.el
@@ -11,12 +11,13 @@
 	 )
        initial-frame-alist))
 
+(mouse-avoidance-mode 'animate)
+
 (setq locale-coding-system 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (set-selection-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
-
 
 (setq x-select-enable-clipboard t)
 (setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
