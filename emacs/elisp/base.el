a1916cdcdf242b1427d88366472ef09a473bb946
Weiming Yin <weiming.yin@gmail.com>
2012-12-12 15:04:46 +0800

    Add more package sources

--- emacs/elisp/base.el
+++ emacs/elisp/base.el
@@ -158,3 +158,8 @@
 
 (golden-ratio-enable)
 
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+(tool-bar-mode -1)
